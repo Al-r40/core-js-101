b9efacb4e6014b16b83d7d8c7f932a4f23662801
Al-r40 <a.v.s.1981rf@gmail.com>
2022-07-22 15:43:30 +0400

    feat: 12 numbers-tasks is done

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -51,8 +51,8 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return value1 / 2 + value2 / 2;
 }
 
 /**
@@ -73,7 +73,7 @@ function getAverage(/* value1, value2 */) {
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
   const a = x1 - x2;
   const b = y1 - y2;
-  const c = Math.sqrt(a * a + b * b);
+  const c = (a ** 2 + b ** 2) ** (1 / 2);
 
   return c;
 }
@@ -114,8 +114,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  return Math.abs(Math.atan2(x1, y1) - Math.atan2(x2, y2));
 }
 
 /**
@@ -187,25 +187,9 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  // const str = `${num}`;
-  // let arr = str.split('');
-
-  // if (pow === 1) {
-  //   arr[3] = 0;
-  // } else if (pow === 2) {
-  //   arr[3] = 0;
-  //   arr[2] = 0;
-  // } else if (pow === 3) {
-  //   arr[3] = 0;
-  //   arr[2] = 0;
-  //   arr[1] = 0;
-  // } else {
-  //   arr = str.split('');
-  // }
-
-  // return +(arr.join(''));
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const number = Math.round(num / (10 ** pow));
+  return number * 10 ** pow;
 }
 
 /**
